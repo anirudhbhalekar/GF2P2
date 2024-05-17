93e76fc09ebcafce258c095782dfb3dccee1d896
anirudhbhalekar <anirudhbhalekar10@gmail.com>
2024-05-17 14:20:41 +0100

    scanner partial

--- logsim/scanner.py
+++ logsim/scanner.py
@@ -26,7 +26,9 @@ class Symbol:
     def __init__(self):
         """Initialise symbol properties."""
         self.type = None
-        self.id = None
+        self.id = None  
+        self.line_number = None
+        self.character = None
 
 
     # Set symbol properties above
@@ -58,17 +60,125 @@ class Scanner:
 
         # read up on initialisation tech
 
+        try: 
+            file = open(path, 'r')
+        except FileNotFoundError: 
+            raise FileNotFoundError
+        
+
+        self.file = file
         self.names = names
+
+        self.line_count = 0 
+
         self.symbol_type_list = [self.COMMA, self.SEMICOLON, self.EQUALS,
-        self.KEYWORD, self.NUMBER, self.NAME, self.EOF] = range(7)
-        self.keywords_list = ["DEFINE", "WITH","input", "initial", "cycle_rep", "CONNECT", "MONITOR", "END"]
-        [self.DEFINE_ID, self.WITH_ID, self.input_ID, self.initial_ID, self.cycle_re_ID, self.CONNECT_ID, self.MONITOR_ID,
-        self.END_ID] = self.names.lookup(self.keywords_list)
+                                self.KEYWORD, self.NUMBER, self.NAME, self.COMMENT, 
+                                self.DOT, self.DEVICE, self.NOTATION, self.PARAM, self.EOF] = range(12)
+        self.keywords_list = ["DEFINE", "WITH", "CONNECT", "MONITOR", "END"]
+        self.param_list = ["input", "initial", "cycle_rep"]
+        self.device_list = ["CLOCK" , "SWITCH" , "NAND" , "AND" , "OR" , "NOR" , "DTYPE" , "XOR"]
+        self.notation_list = ["I", "Q" , "QBAR" , "DATA" , "CLK" , "CLEAR" , "SET"]
+        [self.DEFINE_ID, self.WITH_ID, self.input_ID, self.initial_ID, self.cycle_rep_ID, self.CONNECT_ID, self.MONITOR_ID,
+        self.END_ID] = self.names.lookup(self.keywords_list)     
         self.current_character = ""
 
+        self.file.seek(0)
 
+    def skip_spaces(self): 
+        
+        self.advance()
+        while self.current_character.isspace(): 
+            self.advance()
 
+    def advance(self): 
+        self.current_character = self.file.read(1)
+
+    def get_name(self): 
+
+        name_string = ''
+        name_string += self.current_character
+
+        self.advance()
+
+        while self.current_character.isalnum(): 
+            name_string += self.current_character
+            self.advance()
+            
+        return name_string
+
+    def get_number(self): 
+
+        number_string = ''
+        number_string += self.current_character
+
+        self.advance()
+
+        while self.current_character.isdigit(): 
+            number_string += self.current_character
+            self.advance()
+        
+        return number_string
+    
+        
     def get_symbol(self):
         """Translate the next sequence of characters into a symbol."""
 
         # get the next symbol in the file when called - this is fairly simple to implement
+
+        
+        symbol = Symbol()
+        char_count = len(self.file.read())
+
+        # Find first non-space char
+        self.skip_spaces()
+
+        if self.current_character.isalpha(): 
+            # This is a name
+
+            name_string = self.get_name()
+
+            if name_string in self.keywords_list: 
+                symbol.type = self.KEYWORD
+            else: 
+                symbol.type = self.NAME
+            
+            [symbol.id] = self.names.lookup([name_string])
+
+        elif self.current_character.isdigit(): 
+            
+            # This is a number - we get the number string and pass it as the id
+            number_string = self.get_number()
+            symbol.id = number_string
+            symbol.type = self.NUMBER
+        
+        elif self.current_character == "=": 
+            symbol.type = self.EQUALS
+            self.advance()
+
+        elif self.current_character == ",": 
+            symbol.type = self.COMMA
+            self.advance()
+        
+        elif self.current_character == ".": 
+            symbol.type = self.DOT
+            self.advance()
+
+        elif self.current_character == ";": 
+            symbol.type = self.SEMICOLON
+            self.advance()
+        elif self.current_character == "%":
+            # Comments start and end with a % symbol 
+            symbol.type = self.COMMENT
+            self.advance()
+        elif self.current_character == "\n":
+            # we update line number only and don't pass a symbol 
+            self.line_count += 1
+
+        else: 
+            # Invalid character just pass over
+            self.advance()
+
+        symbol.line_number = self.line_count
+        symbol.character = self.file.tell()
+
+        return symbol 
