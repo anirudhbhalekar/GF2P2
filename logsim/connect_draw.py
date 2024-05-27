fc4cf48dbfbecb734f456352061717512cbae0d9
anirudhbhalekar <anirudhbhalekar10@gmail.com>
2024-05-27 21:44:46 +0100

    connect draw update

--- logsim/connect_draw.py
+++ logsim/connect_draw.py
@@ -11,26 +11,51 @@ from logic_draw import LogicDrawer
 class ConnectDrawer:
     """Handle all Connections drawings."""
     
-    def __init__(self, connection_list: list):
+    def __init__(self, connection_list: list, domain_dict: dict):
         
         # We receive connection_list in the form (draw_obj, outgoing id, draw_obj, input_id)
         self.connection_list = connection_list
+
+        # Stores dict of all min_max coords (bounding box) for all operators
+        self.domain_dict = domain_dict
         
 
     def draw_all_connections(self): 
 
         for connection_def in self.connection_list: 
+            self.draw_one_connection(connection_def)
+    
+    def draw_one_connection(self, connection_def): 
+        
+        inp_obj = connection_def[0] 
+        out_obj = connection_def[2]
 
-            inp_obj = connection_def[0] 
-            out_obj = connection_def[2]
+        inp_id = connection_def[1]
+        out_id = connection_def[3]
 
-            inp_id = connection_def[1]
-            out_id = connection_def[3]
+        inp_domain = self.domain_dict[inp_obj]
+        out_domain = self.domain_dict[out_obj]
 
-            (start_x, start_y) = inp_obj.input_list[inp_id]
-            (end_x, end_y) = out_obj.output_list[out_id]
+        num_inputs = len(inp_obj.input_list)
+        num_outputs = len(out_obj.output_list) 
 
-            d
+        (start_x, start_y) = inp_obj.input_list[inp_id]
+        (end_x, end_y) = out_obj.output_list[out_id]
+
+        if inp_id + 1 < num_inputs/2: 
+            # We will go down and to the left
+            d_coord = inp_domain[0]
+
+        
+
+        glBegin(GL_LINE_STRIP)
+        glVertex2f(start_x, start_y)
+        glVertex2f(d_coord[0], start_y)
+        glVertex2f(d_coord[0], d_coord[1])
+
+        MAX_Y = 500
+        MIN_Y = -500
+
 
             
 
