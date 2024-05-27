cbbff5015bba3d9f71a591dea8ef1a72abfbe332
anirudhbhalekar <anirudhbhalekar10@gmail.com>
2024-05-27 14:57:53 +0100

    addition of connect_draw

---
+++ logsim/connect_draw.py
@@ -0,0 +1,13 @@
+from math import cos, sin, pi
+from OpenGL.GL import glBegin, glEnd, glVertex2f, glColor3f, GL_LINE_STRIP, GL_LINE_LOOP
+
+
+
+class ConnectDrawer:
+    """Handle all Connections drawings."""
+    
+    def __init__(self, name, x, y, n_iter=10, n_inputs=2):
+        
+        pass
+    
+        
