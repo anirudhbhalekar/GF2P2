e32fc32460d1134ca650aff3f944261d0c1a7ae9
Shawn Li <syl52@cam.ac.uk>
2024-05-27 10:22:31 +0100

    revert to working version

--- logsim/gui.py
+++ logsim/gui.py
@@ -12,7 +12,7 @@ import wx
 import wx.glcanvas as wxcanvas
 from math import cos, sin, pi
 from OpenGL import GL, GLUT
-from OpenGL.GL import glBegin, glEnd, glVertex2f, glColor3f, GL_LINE_STRIP, GL_TRIANGLE_FAN, GL_LINE_LOOP, GL_POLYGON
+from OpenGL.GL import glBegin, glEnd, glVertex2f, glColor3f, GL_LINE_STRIP, GL_TRIANGLE_FAN, GL_LINE_LOOP
 
 from names import Names
 from devices import Devices
@@ -75,8 +75,7 @@ class LogicGateDrawer:
         LogicGateDrawer.draw_and_gate(self)
 
         # Draw the circle for the NOT part, radius 5
-        glBegin(GL_POLYGON)
-    
+        glBegin(GL_LINE_LOOP)
         for i in range(self.n_iter):
             angle = 2 * pi * i / float(self.n_iter)
             # Must add radius to x length for x1 argument
@@ -85,7 +84,6 @@ class LogicGateDrawer:
             x1 = r * cos(angle) + self.x + self.length + (self.height / 2) + r
             y1 = r * sin(angle) + self.y + (self.height / 2)
             glVertex2f(x1, y1)
-        
         glEnd()
 
     
@@ -157,25 +155,6 @@ class LogicGateDrawer:
         glVertex2f(self.x - 10, self.y + self.height)
         glVertex2f(self.x - 20, self.y + self.height + 10)
         glEnd()
-        LogicGateDrawer.draw_or_gate(self.x + 10, self.y)
-
-
-class ConnectionDrawer: 
-
-    def __init__(self, connect_list):
-        """ Takes connection list of objects"""
-        self.connect_list = connect_list
-
-    def make_connection(self): 
-        """ Connection List - name to name"""
-        
-        for tup in self.connect_list: 
-            input_obj = tup[0] # This is the drawer object
-            output_obj = tup[1] 
-
-           
-            # Lets have a property of the drawer class to be self.input_list - which is a list of input x,y coords 
-
 
 class MyGLCanvas(wxcanvas.GLCanvas):
     """Handle all drawing operations.
@@ -432,8 +411,7 @@ class Gui(wx.Frame):
 
     def __init__(self, title, path, names, devices, network, monitors):
         """Initialise widgets and layout."""
-        self.w, self.h = 800, 600
-        super().__init__(parent=None, title=title, size=(self.w, self.h))
+        super().__init__(parent=None, title=title, size=(800, 600))
 
         # Configure the file menu
         menuBar = wx.MenuBar()
@@ -489,31 +467,6 @@ class Gui(wx.Frame):
         self.SetSizeHints(600, 600)
         self.SetSizer(main_sizer)
 
-    
-    def GetRoundBitmap(self, w, h, r ):
-        """Gets the round """
-        maskColor = wx.Color(0,0,0)
-        shownColor = wx.Color(5,5,5)
-        b = wx.EmptyBitmap(w,h)
-        dc = wx.MemoryDC(b)
-        dc.SetBrush(wx.Brush(maskColor))
-        dc.DrawRectangle(0,0,w,h)
-        dc.SetBrush(wx.Brush(shownColor))
-        dc.SetPen(wx.Pen(shownColor))
-        dc.DrawRoundedRectangle(0,0,w,h,r)
-        dc.SelectObject(wx.NullBitmap)
-        b.SetMaskColour(maskColor)
-        return b
-
-    def GetRoundShape(self,w, h, r ):
-        return wx.RegionFromBitmap(self.GetRoundBitmap(w,h,r) )
-    
-    def SetRoundShape(self, event=None):
-        w, h = self.GetSizeTuple()
-        self.SetShape(self.GetRoundShape( w,h, 10 ) )
-    
-    
-    
     def on_menu(self, event):
         """Handle the event when the user selects a menu item."""
         Id = event.GetId()
