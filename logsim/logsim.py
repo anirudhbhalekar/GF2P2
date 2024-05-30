01cc6c1843de37bf2f3bac2a28c527c67edbaf69
Shawn Li <syl52@cam.ac.uk>
2024-05-30 23:42:57 +0100

    Translation first step, create pot file from logsim.py

--- logsim/logsim.py
+++ logsim/logsim.py
@@ -1,19 +1,8 @@
-#!/usr/bin/env python3
-"""Parse command line options and arguments for the Logic Simulator.
-
-This script parses options and arguments specified on the command line, and
-runs either the command line user interface or the graphical user interface.
-
-Usage
------
-Show help: logsim.py -h
-Command line user interface: logsim.py -c <file path>
-Graphical user interface: logsim.py <file path>
-"""
 import getopt
 import sys
 
 import wx
+import gettext
 
 from names import Names
 from devices import Devices
@@ -24,6 +13,9 @@ from parse import Parser
 from userint import UserInterface
 from gui_3D import Gui
 
+# Initialize gettext translation
+gettext.install('logsim', localedir='locales')
+_ = gettext.gettext
 
 def main(arg_list):
     """Parse the command line options and arguments specified in arg_list.
@@ -31,14 +23,14 @@ def main(arg_list):
     Run either the command line user interface, the graphical user interface,
     or display the usage message.
     """
-    usage_message = ("Usage:\n"
-                     "Show help: logsim.py -h\n"
-                     "Command line user interface: logsim.py -c <file path>\n"
-                     "Graphical user interface: logsim.py <file path>")
+    usage_message = _("""Usage:
+Show help: logsim.py -h
+Command line user interface: logsim.py -c <file path>
+Graphical user interface: logsim.py <file path>""")
     try:
         options, arguments = getopt.getopt(arg_list, "hc:")
     except getopt.GetoptError:
-        print("Error: invalid command line arguments\n")
+        print(_("Error: invalid command line arguments\n"))
         print(usage_message)
         sys.exit()
 
@@ -47,11 +39,6 @@ def main(arg_list):
     devices = Devices(names)
     network = Network(names, devices)
     monitors = Monitors(names, devices, network)
-
-    # names = None
-    # devices = None
-    # network = None
-    # monitors = None
 
     for option, path in options:
         if option == "-h":  # print the usage message
@@ -68,7 +55,7 @@ def main(arg_list):
     if not options:  # no option given, use the graphical user interface
 
         if len(arguments) != 1:  # wrong number of arguments
-            print("Error: one file path required\n")
+            print(_("Error: one file path required\n"))
             print(usage_message)
             sys.exit()
 
@@ -79,7 +66,7 @@ def main(arg_list):
         assert parser.parse_network()
         # Initialise an instance of the gui.Gui() class
         app = wx.App()
-        gui = Gui("Logic Simulator", path, names, devices, network,
+        gui = Gui(_("Logic Simulator"), path, names, devices, network,
                     monitors)
         gui.Show(True)
         app.MainLoop()
