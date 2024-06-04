925bd29c3260117806e136efcd9f61b2942f3202
Shawn Li <syl52@cam.ac.uk>
2024-06-04 18:22:21 +0100

    change lang back to en if 3rd language not en or el deteched

--- logsim/names.py
+++ logsim/names.py
@@ -25,6 +25,12 @@ if len(sys.argv) > 2:
 if os.getenv("LANG") == "el_GR.UTF-8":
     locale = "el_GR.utf8"
     #print("Greek system language detected")
+elif os.getenv("LANG") == "en_US.UTF-8" or os.getenv("LANG") == "en_GB.UTF-8":
+    #print("Your system language is English.")
+    locale = "en_GB.utf8"
+else:
+    #print("Attention - your system language is neither English nor Greek. Logsim will run in English.")
+    locale = "en_GB.utf8"
 lang = gettext.translation("logsim", localedir=os.path.join(os.path.dirname(__file__), 'locales'), languages=[locale], fallback=True)
 lang.install()
 _ = lang.gettext
